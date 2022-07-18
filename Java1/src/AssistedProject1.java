fb029569a0b08fe5a7d15597cc1bd76c41ca9b4d
Om Sreeja <pathuri.omsrija@gmail.com>
2022-07-18 14:20:22 +0530

    typecasting

--- Java1/src/AssistedProject1.java
+++ Java1/src/AssistedProject1.java
@@ -4,20 +4,32 @@ public class AssistedProject1 {
 			
 			//implicit conversion
 			System.out.println("Implicit Type Casting");
-			char a='A';
+			int a=10;
 			System.out.println("Value of a: "+a);
 			
-			int b=a;
+			float b=a;
 			System.out.println("Value of b: "+b);
 			
-			float c=a;
+			long c=a;
 			System.out.println("Value of c: "+c);
 			
-			long d=a;
+			double d=a;
 			System.out.println("Value of d: "+d);
-			
-			double e=a;
+			System.out.println("\n");
+			System.out.println("Implicity converting char datatype to other datatypes");
+			char e='B';
 			System.out.println("Value of e: "+e);
+			int f=e;
+			System.out.println("Value of f: "+f);
+			
+			float g=e;
+			System.out.println("Value of g: "+g);
+			
+			long h=e;
+			System.out.println("Value of h: "+h);
+			
+			double i=e;
+			System.out.println("Value of i: "+i);
 			
 					
 			System.out.println("\n");
@@ -29,6 +41,10 @@ public class AssistedProject1 {
 			int y=(int)x;
 			System.out.println("Value of x: "+x);
 			System.out.println("Value of y: "+y);
+			float z=100.11f;
+			char w =(char)z;
+			System.out.println("Value of z: "+z);
+			System.out.println("Value of w: "+w);
 			
 		}
 	}
