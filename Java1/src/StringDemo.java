7e385d72cdaedb9deedcc53595e98faec2c798b1
Om Sreeja <pathuri.omsrija@gmail.com>
2022-07-19 17:33:51 +0530

    practice programs

---
+++ Java1/src/StringDemo.java
@@ -0,0 +1,32 @@
+import java.lang.*;
+public class StringDemo{
+	public static void main(String args[])
+	{
+		//Creating a String
+		String s=new String("Hello,Welcome to java");
+		//implementing methods in java
+		System.out.println("length of string s is :"+s.length());
+		System.out.println("UpperCase of string s is :"+s.toUpperCase());
+		String s1="Hello";
+		String s2="Hello";
+		//comapring of two strings s1 and s2
+		System.out.println("string comparison :"+s1.compareTo(s2));
+		//substring
+		System.out.println("subtsring of s :"+s.substring(2));
+		//equals
+		System.out.println("string equals : "+s.equals(s1));
+		//String Builder
+		System.out.println("\n");
+		System.out.println("String to StringBuilder");
+		StringBuilder sb=new StringBuilder(s);
+		System.out.println(sb);
+		//String Buffer
+				System.out.println("\n");
+				System.out.println("String to StringBuffer");
+				StringBuffer bf=new StringBuffer(s1);
+				bf.append("Nice to meet you!");
+				System.out.println(bf);
+				
+	}
+
+}
